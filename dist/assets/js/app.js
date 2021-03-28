630561659d14fa67e15a438af2d001337f809d77
Ilya-d-web <iliya.grishunin@gmail.com>
2021-03-28 22:02:47 +0300

    Клонирование локального проекта Night Watches в репозиторий

---
+++ dist/assets/js/app.js
@@ -0,0 +1,144 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/assets/js/app.js":
+/*!******************************!*\
+  !*** ./src/assets/js/app.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("\n\n//# sourceURL=webpack:///./src/assets/js/app.js?");
+
+/***/ }),
+
+/***/ "./src/assets/js/menu-header.js":
+/*!**************************************!*\
+  !*** ./src/assets/js/menu-header.js ***!
+  \**************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("$(document).ready(function () {\r\n    let scrollOffset = 0;\r\n\r\n    $('.icon-menu').on(\"click\", function () {\r\n        toggleClassActive();\r\n        //Смена класса active у элементов menu и icon\r\n\r\n        if ($('.menu__body').hasClass('active')) {\r\n            stopPropagationIcon();\r\n            //Остановка клика по icon-menu\r\n            stopPropagationMenu();\r\n            //Остановка клика по menu__body\r\n\r\n            $('.header__content').on(\"click\", function (event) {\r\n                removeClassActive();\r\n            });\r\n            //Закрытие burger кликом по header\r\n        }\r\n    });\r\n\r\n    $(window).on(\"resize\", function () {\r\n        if (window.matchMedia('(min-width: 768px)').matches) {\r\n            removeClassActive();\r\n        }\r\n    });\r\n    //Проверка экрана на смену ширины\r\n\r\n    $(window).on(\"scroll\", function () {\r\n        let beginH = $(\"#begin\").innerHeight();\r\n        scrollOffset = $(this).scrollTop();\r\n\r\n        if (scrollOffset > beginH - 38) {\r\n            $('.header').addClass('fixed');\r\n        } else {\r\n            $('.header').removeClass('fixed');\r\n        }\r\n    });\r\n    //Фиксирование позиции burger во время скрола\r\n\r\n    $('a.menu__link').on(\"click\", function () {\r\n        removeClassActive();\r\n        //Закрытие burger при прокрутке к якорю\r\n\r\n        $(\"html, body\").animate({\r\n            scrollTop: ($($(this).attr(\"href\")).offset().top + -80) + \"px\"\r\n        }, {\r\n            duration: 1000,\r\n            easing: \"swing\",\r\n        });\r\n    });\r\n    //Плавная прокрутка\r\n});\r\n//Главная функция страницы, которая срабатывает после загрузки содержимого\r\n\r\nfunction removeClassActive() {\r\n    $('.icon-menu').removeClass('active');\r\n    $('.menu__body').removeClass('active');\r\n    $('body').removeClass('no-scroll');\r\n}\r\n//Удаление класса active\r\nfunction toggleClassActive() {\r\n    $('.icon-menu').toggleClass('active');\r\n    $('.menu__body').toggleClass('active');\r\n    $('body').toggleClass('no-scroll');\r\n}\r\n//Смена класса active\r\nfunction stopPropagationIcon() {\r\n    $('.icon-menu').on(\"click\", function (event) {\r\n        event.stopPropagation();\r\n    });\r\n}\r\n//Остановка клика по icon-menu\r\nfunction stopPropagationMenu() {\r\n    $('.menu__body').on(\"click\", function (event) {\r\n        event.stopPropagation();\r\n    });\r\n}\r\n//Остановка клика по menu__body\n\n//# sourceURL=webpack:///./src/assets/js/menu-header.js?");
+
+/***/ }),
+
+/***/ "./src/assets/js/slider.js":
+/*!*********************************!*\
+  !*** ./src/assets/js/slider.js ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("$(document).ready(function () {\r\n    $('.slider').slick({\r\n        arrows: true,\r\n        slidesToShow: 1,\r\n        slidesToScroll: 1,\r\n        speed: 400,\r\n        easing: 'ease-in-out',\r\n        autoplay: true,\r\n        autoplaySpeed: 3500,\r\n        draggable: false,\r\n        touchThreshold: 8,\r\n        waitForAnimate: true,\r\n        variableWidth: true,\r\n        responsive: [{\r\n            breakpoint: 767.98,\r\n            settings: {\r\n                arrows: false,\r\n            },\r\n        }],\r\n    });\r\n})\n\n//# sourceURL=webpack:///./src/assets/js/slider.js?");
+
+/***/ }),
+
+/***/ "./src/assets/js/webpForCss.js":
+/*!*************************************!*\
+  !*** ./src/assets/js/webpForCss.js ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("function testWebP(callback) {\r\n\r\n    var webP = new Image();\r\n    webP.onload = webP.onerror = function () {\r\n        callback(webP.height == 2);\r\n    };\r\n    webP.src = \"data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA\";\r\n}\r\n\r\ntestWebP(function (support) {\r\n\r\n    if (support == true) {\r\n        document.querySelector('body').classList.add('webp');\r\n    } else {\r\n        document.querySelector('body').classList.add('no-webp');\r\n    }\r\n});\n\n//# sourceURL=webpack:///./src/assets/js/webpForCss.js?");
+
+/***/ }),
+
+/***/ 0:
+/*!***************************************************************************************************************************!*\
+  !*** multi ./src/assets/js/app.js ./src/assets/js/menu-header.js ./src/assets/js/slider.js ./src/assets/js/webpForCss.js ***!
+  \***************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("__webpack_require__(/*! E:\\Any\\WEB\\ПРОЕКТ --Night Watch--\\src\\assets\\js\\app.js */\"./src/assets/js/app.js\");\n__webpack_require__(/*! E:\\Any\\WEB\\ПРОЕКТ --Night Watch--\\src\\assets\\js\\menu-header.js */\"./src/assets/js/menu-header.js\");\n__webpack_require__(/*! E:\\Any\\WEB\\ПРОЕКТ --Night Watch--\\src\\assets\\js\\slider.js */\"./src/assets/js/slider.js\");\nmodule.exports = __webpack_require__(/*! E:\\Any\\WEB\\ПРОЕКТ --Night Watch--\\src\\assets\\js\\webpForCss.js */\"./src/assets/js/webpForCss.js\");\n\n\n//# sourceURL=webpack:///multi_./src/assets/js/app.js_./src/assets/js/menu-header.js_./src/assets/js/slider.js_./src/assets/js/webpForCss.js?");
+
+/***/ })
+
+/******/ });
---
+++ gulpfile.js
@@ -0,0 +1,346 @@
+"use strict";
+
+//!<Начальные параметры проекта>
+//<Объявление всех модулей>
+const { src, dest } = require("gulp"),
+    gulp = require("gulp"),
+    autoprefixer = require("gulp-autoprefixer"),
+    cssbeautify = require("gulp-cssbeautify"),
+    removeComments = require("gulp-strip-css-comments"),
+    rename = require("gulp-rename"),
+    sass = require("gulp-sass"),
+    cssnano = require("gulp-cssnano"),
+    uglify = require("gulp-uglify"),
+    plumber = require("gulp-plumber"),
+    panini = require("panini"),
+    imagemin = require("gulp-imagemin"),
+    del = require("del"),
+    notify = require("gulp-notify"),
+    webpack = require("webpack"),
+    webpackStream = require("webpack-stream"),
+    ttf2woff = require("gulp-ttf2woff"),
+    ttf2woff2 = require("gulp-ttf2woff2"),
+    fonter = require("gulp-fonter"),
+    webp = require("gulp-webp"),
+    webpHtml = require("gulp-webp-html"),
+    webpCss = require("gulp-webpcss"),
+    browserSync = require("browser-sync").create();
+
+//<Папка src и dist>
+const srcPath = "src/";
+const distPath = "dist/";
+
+const fs = require('fs');
+
+//<Пути к файлам>
+const path = {
+    build: {
+        html: distPath,
+        js: distPath + "assets/js/",
+        css: distPath + "assets/css/",
+        images: distPath + "assets/images/",
+        fonts: distPath + "assets/fonts/",
+    },
+    src: {
+        html: srcPath + "*.html",
+        js: srcPath + "assets/js/*.js",
+        css: srcPath + "assets/scss/*.scss",
+        images:
+            srcPath +
+            "assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
+        fonts: srcPath + "assets/fonts/*.ttf",
+    },
+    watch: {
+        html: srcPath + "**/*.html",
+        js: srcPath + "assets/js/**/*.js",
+        css: srcPath + "assets/scss/**/*.scss",
+        images:
+            srcPath +
+            "assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
+    },
+    clean: "./" + distPath,
+};
+
+//!<Функции>
+//<Создание локального сервера>
+function serve() {
+    browserSync.init({
+        server: {
+            baseDir: "./" + distPath,
+        },
+    });
+}
+
+//<Сборка HTML>
+function html(cb) {
+    panini.refresh();
+    return src(path.src.html, { base: srcPath })
+        .pipe(plumber())
+        .pipe(
+            panini({
+                root: srcPath,
+                layouts: srcPath + "layouts/",
+                partials: srcPath + "partials/",
+                helpers: srcPath + "helpers/",
+                data: srcPath + "data/",
+            })
+        )
+        .pipe(webpHtml())
+        .pipe(dest(path.build.html))
+        .pipe(browserSync.reload({ stream: true }));
+
+    cb();
+}
+
+//<Сборка SCSS>
+function css(cb) {
+    return src(path.src.css, { base: srcPath + "assets/scss/" })
+        .pipe(
+            plumber({
+                errorHandler: function (err) {
+                    notify.onError({
+                        title: "SCSS Error",
+                        message: "Error: <%= error.message %>",
+                    })(err);
+                    this.emit("end");
+                },
+            })
+        )
+        .pipe(
+            sass({
+                includePaths: "./node_modules/",
+            })
+        )
+        .pipe(
+            autoprefixer({
+                cascade: true,
+            })
+        )
+        .pipe(cssbeautify())
+        .pipe(webpCss({
+            webpClass: '.webp',
+            noWebpClass: '.no-webp'
+        }))
+        .pipe(dest(path.build.css))
+        .pipe(
+            cssnano({
+                zindex: false,
+                discardComments: {
+                    removeAll: true,
+                },
+            })
+        )
+        .pipe(removeComments())
+        .pipe(
+            rename({
+                suffix: ".min",
+                extname: ".css",
+            })
+        )
+        .pipe(dest(path.build.css))
+        .pipe(browserSync.reload({ stream: true }));
+
+    cb();
+}
+
+//<Просмотр событий SCSS>
+function cssWatch(cb) {
+    return src(path.src.css, { base: srcPath + "assets/scss/" })
+        .pipe(
+            plumber({
+                errorHandler: function (err) {
+                    notify.onError({
+                        title: "SCSS Error",
+                        message: "Error: <%= error.message %>",
+                    })(err);
+                    this.emit("end");
+                },
+            })
+        )
+        .pipe(
+            sass({
+                includePaths: "./node_modules/",
+            })
+        )
+        .pipe(
+            rename({
+                suffix: ".min",
+                extname: ".css",
+            })
+        )
+        .pipe(dest(path.build.css))
+        .pipe(browserSync.reload({ stream: true }));
+
+    cb();
+}
+
+//<Сборка JS>
+function js(cb) {
+    return src(path.src.js, { base: srcPath + "assets/js/" })
+        .pipe(
+            plumber({
+                errorHandler: function (err) {
+                    notify.onError({
+                        title: "JS Error",
+                        message: "Error: <%= error.message %>",
+                    })(err);
+                    this.emit("end");
+                },
+            })
+        )
+        .pipe(
+            webpackStream({
+                mode: "production",
+                output: {
+                    filename: "app.js",
+                },
+                module: {
+                    rules: [
+                        {
+                            test: /\.(js)$/,
+                            exclude: /(node_modules)/,
+                            loader: "babel-loader",
+                            query: {
+                                presets: ["@babel/preset-env"],
+                            },
+                        },
+                    ],
+                },
+            })
+        )
+        .pipe(dest(path.build.js))
+        .pipe(browserSync.reload({ stream: true }));
+
+    cb();
+}
+
+//<Просмотр событий JS>
+function jsWatch(cb) {
+    return src(path.src.js, { base: srcPath + "assets/js/" })
+        .pipe(
+            plumber({
+                errorHandler: function (err) {
+                    notify.onError({
+                        title: "JS Error",
+                        message: "Error: <%= error.message %>",
+                    })(err);
+                    this.emit("end");
+                },
+            })
+        )
+        .pipe(
+            webpackStream({
+                mode: "development",
+                output: {
+                    filename: "app.js",
+                },
+            })
+        )
+        .pipe(dest(path.build.js))
+        .pipe(browserSync.reload({ stream: true }));
+
+    cb();
+}
+
+//<Сжатие IMG>
+function images(cb) {
+    return src(path.src.images)
+        .pipe(
+            webp({
+                quality: 75
+            })
+        )
+        .pipe(dest(path.build.images))
+        .pipe(src(path.src.images))
+        .pipe(
+            imagemin([
+                imagemin.gifsicle({ interlaced: true }),
+                imagemin.mozjpeg({ quality: 95, progressive: true }),
+                imagemin.optipng({ optimizationLevel: 5 }),
+                imagemin.svgo({
+                    plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+                }),
+            ])
+        )
+        .pipe(dest(path.build.images))
+        .pipe(browserSync.reload({ stream: true }));
+
+    cb();
+}
+
+//<Конвертирование TTF в WOFF, WOFF2>
+function fonts() {
+    src(path.src.fonts)
+        .pipe(ttf2woff())
+        .pipe(dest(path.build.fonts));
+    return src(path.src.fonts)
+        .pipe(ttf2woff2())
+        .pipe(dest(path.build.fonts));
+}
+
+//<Конвертирование OTF в TTF(использовать как отдельную команду --gulp otf2ttf--)>
+gulp.task('otf2ttf', function () {
+    return src([srcPath + 'assets/fonts/*.otf'])
+        .pipe(fonter({
+            formats: ['ttf']
+        }))
+        .pipe(dest(srcPath + 'assets/fonts/'));
+});
+
+//<Подключение шрифтов в файл fonts.scss>
+function fontsStyle(params) {
+    let file_content = fs.readFileSync(srcPath + 'assets/scss/_fonts.scss');
+    if (file_content == '') {
+        fs.writeFile(srcPath + 'assets/scss/_fonts.scss', '', cb1);
+        return fs.readdir(path.build.fonts, function (err, items) {
+            if (items) {
+                let c_fontname;
+                for (var i = 0; i < items.length; i++) {
+                    let fontname = items[i].split('.');
+                    fontname = fontname[0];
+                    if (c_fontname != fontname) {
+                        fs.appendFile(srcPath + 'assets/scss/_fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb1);
+                    }
+                    c_fontname = fontname;
+                }
+            }
+        })
+    }
+}
+
+function cb1() {
+
+}
+
+//<Очистка>
+function clean(cb) {
+    return del(path.clean);
+
+    cb();
+}
+
+//<Просмотр событий>
+function watchFiles() {
+    gulp.watch([path.watch.html], html);
+    gulp.watch([path.watch.css], cssWatch);
+    gulp.watch([path.watch.js], jsWatch);
+    gulp.watch([path.watch.images], images);
+}
+
+//!<Сборка проекта>
+//<сборка экспортированных функций, установка на них просмотрщика событий и запуск локального сервера>
+const build = gulp.series(clean, gulp.parallel(html, css, js, images, fonts), fontsStyle);
+const watch = gulp.parallel(build, watchFiles, serve);
+
+
+//<Экспорт функций>
+exports.css = css;
+exports.fontsStyle = fontsStyle;
+exports.fonts = fonts;
+exports.images = images;
+exports.js = js;
+exports.html = html;
+exports.clean = clean;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
